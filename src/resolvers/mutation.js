69af4b8ca2f4a2bc6d125f95dc4f1dacfe9d2af8
harisw <hari14@mhs.if.its.ac.id>
2022-03-05 15:56:37 +0700

    initial commit

---
+++ src/resolvers/mutation.js
@@ -0,0 +1,135 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+    AuthenticationError,
+    ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+const mongoose = require('mongoose');
+
+module.exports = {
+    newNote: async (parent, args, {models, user }) => {
+        if(!user) {
+            throw new AuthenticationError('You must be signed in');
+        }
+
+        return await models.Note.create({
+            content: args.content,
+            author: mongoose.Types.ObjectId(user.id)
+        });
+    },
+    updateNote: async (parent, { content, id }, {models, user}) => {
+        if(!user){
+            throw new AuthenticationError('Need to signed in');
+        }
+        const note = await models.Note.findById(id);
+        if(note && String(note.author) !== user.id){
+            throw new ForbiddenError("You're not the owner");
+        }
+
+        return await models.Note.findOneAndUpdate(
+            {
+                _id: id
+            },
+            {
+                $set: {
+                    content
+                }
+            },
+            {
+                new: true
+            }
+        );
+    },
+    deleteNote: async (parent, { id }, {models, user}) => {
+        if(!user){
+            throw new AuthenticationError('Need to signed in');
+        }
+        const note = await models.Note.findById(id);
+        if(note && String(note.author) !== user.id){
+            throw new ForbiddenError("You're not the owner");
+        }
+
+        try {
+            await note.remove();
+            return true;
+        } catch (err) {
+            return false;
+        }
+    },
+    signUp: async (parent, {username, email, password}, { models}) => {
+        email = email.trim().toLowerCase();
+        const hashed = await bcrypt.hash(password, 10);
+        const avatar = gravatar(email);
+        try {
+            const user = await models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+            return jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+        } catch (err) {
+            console.log(err);
+            throw new Error('Error creating account');
+        }
+    },
+    signIn: async (parent, {username, email, password}, { models}) => {
+        if(email) {
+            email = email.trim().toLowerCase();
+        }
+        const user = await models.User.findOne({
+            $or: [{ email}, {username}]
+        });
+
+        if(!user){
+            throw new AuthenticationError('Error signing in');
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid){
+            throw new AuthenticationError('Error signing in');
+        }
+        return jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+    },
+    toggleFavorite: async (parent, { id }, {models, user}) => {
+        if(!user){
+            throw new AuthenticationError();
+        }
+        let noteCheck = await models.Note.findById(id);
+        const hasUser = noteCheck.favoritedBy.indexOf(user.id);
+
+        if(hasUser >= 0) {
+            return await models.Note.findByIdAndUpdate(
+                id,
+                {
+                    $pull: {
+                        favoritedBy: mongoose.Types.ObjectId(user.id)
+                    },
+                    $inc: {
+                        favoriteCount: -1
+                    }
+                },
+                {
+                    new: true
+                }
+            );
+        } else {
+            return await models.Note.findByIdAndUpdate(
+                id,
+                {
+                    $push: {
+                        favoritedBy: mongoose.Types.ObjectId(user.id)
+                    },
+                    $inc: {
+                        favoriteCount: 1
+                    }
+                },
+                {
+                    new: true
+                }
+            )
+        }
+    }
+};
